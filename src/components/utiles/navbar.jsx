52c94f782a70b28663bcc62c6ab142d0259c75d4
Bryan Raschella <bryanraschella2701@hotmail.com>
2021-10-14 22:50:41 -0300

    Creando barra

--- src/components/utiles/navbar.jsx
+++ src/components/utiles/navbar.jsx
@@ -5,16 +5,14 @@ import "../styles/navbar.scss";
 const NavBar = ()=> {
 return(
 <>
-<nav className="nav">
-    <div className="menuTitle">
-        <h1>Titulo</h1>
-        </div>
-        <ul className="navUl">
-            <li className="navLi"><a href="*" className="a">Inicio</a></li>
-            <li className="navLi"><a href="*" className="a">Tienda</a></li>
-            <li className="navLi"><a href="*" className="a">Promociones</a></li>
-            <li className="navLi"><a href="*" className="a">Contacto</a></li>
-
+<nav className="nav ">
+        <ul className="navUl menu">
+        <li className="logo"><a href="#"><img className="logo" src="https://android.appsapk.com/wp-content/uploads/2020/03/PS5-logo-revealed-at-CES-2020-1024x556.jpg" alt="logo" /></a></li>
+            <li className="navLi item"><a href="*" className="a">Inicio</a></li>
+            <li className="navLi item"><a href="*" className="a">Tienda</a></li>
+            <li className="navLi item"><a href="*" className="a">Promociones</a></li>
+            <li className="navLi item"><a href="*" className="a">Contacto</a></li>
+            <li className="toggle"><a href="#"><i className="fas fa-bars"></i></a></li>
         </ul>
 </nav>
 </>
